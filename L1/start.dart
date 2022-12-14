fd686e3e03425087df205b8b1a435c12189b5006
Cem Kanpalta <ckanpalta@gmail.com>
2022-12-14 09:23:05 +0300

    degisken atama eklendi

--- L1/start.dart
+++ L1/start.dart
@@ -5,4 +5,16 @@ void main() {
 
   // yan yana yazdırmak icin
   print("Cem " + "Kanpalta");
+
+  // yan yana yazdırmak icin
+  print("Cem " + "Kanpalta " + "Yaş " + "30");
+
+  // degisken tanımlama
+  String name = "Cem";
+  print(name);
+
+  int money = 50;
+  print(money);
+
+  print(name + ' ' + '$money');
 }
---
+++ L1/user_basic.dart
@@ -0,0 +1,11 @@
+void main() {
+  // musterinin adi
+  // musterinin parasi
+  // musteriye merhaba de parasini soyle
+  // +5 tl ekle
+  String userName = "Cem";
+  int userMoney = 50;
+  userMoney = userMoney + 5;
+
+  print("Merhaba  $userName  $userMoney");
+}

b0714c25ea398ef7a52deff6acab95ee3eb15a92
Cem Kanpalta <ckanpalta@gmail.com>
2022-12-14 13:47:11 +0300

    Update user_adv.dart

--- L1/user_adv.dart
+++ L1/user_adv.dart
@@ -20,4 +20,20 @@ void main() {
   int bankUser2Money = 500;
   bankUser2Money = bankUser2Money - bankUser1Money.toInt();
   print('User 2 money: $bankUser2Money');
+
+  // benim bir bi halı saha var 100 kisi kapasiteli
+  // saat 10 da 20 kisilik bir grup mac yapacak ve 20 kesin gelecek
+  // saat 10 da musteri 2 gelip 60 kisilik yer ayırtacak
+  // bu islem sonrasında benim hali saha kapasitem kac kisilik kalmistir
+  // int sum =musteri1 + musteri2
+  // print total(costn) - sum
+
+  const int kapasite = 100;
+  const String musteri1 = "Musteri 1";
+  const String musteri2 = "Musteri 2";
+  int musteri1Kisi = 20;
+  int musteri2Kisi = 50;
+  int sum = musteri1Kisi + musteri2Kisi;
+  int kalan = kapasite - sum;
+  print('kalan kapasite $kalan');
 }

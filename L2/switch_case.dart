fb0eccd2c104178d0315c4046a29aea0a26c622e
Cem Kanpalta <ckanpalta@gmail.com>
2022-12-15 13:38:53 +0300

    Create switch_case.dart

---
+++ L2/switch_case.dart
@@ -0,0 +1,23 @@
+void main() {
+  // 2 bravo
+  // 1 yeterli
+  // 0 basarisiz
+  final int classDegree = 2;
+  bool isSucess = false;
+
+  switch (classDegree) {
+    case 2:
+      print('Bravo');
+      isSucess = true;
+      break;
+    case 1:
+      print('Yeterli');
+      isSucess = true;
+      break;
+    case 0:
+      print('Basarisiz');
+      isSucess = false;
+      break;
+  }
+  print('Ogrencinin sonucu: $isSucess');
+}

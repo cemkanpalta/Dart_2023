28f6cb969390c64b65f64aca16bc01d1a478706c
Cem Kanpalta <ckanpalta@gmail.com>
2022-12-20 13:59:50 +0300

    Update operators.dart

--- L2/operators.dart
+++ L2/operators.dart
@@ -1,1 +1,7 @@
-void main() {}
+void main() {
+  const int appleMoney = 20;
+  const double discount = 2.5;
+  int myMoney = 30;
+  myMoney = myMoney - (appleMoney ~/ discount);
+  print(myMoney);
+}

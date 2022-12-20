b08e504b469eaf7a850eb3c3a5b454eec19d332d
Cem Kanpalta <ckanpalta@gmail.com>
2022-12-20 08:26:00 +0300

    Update switch_case.dart

--- L2/switch_case.dart
+++ L2/switch_case.dart
@@ -19,8 +19,35 @@ void main() {
       isSucess = false;
       break;
     default:
-      print('Başarısız');
+      print('Basarisiz');
       isSucess = false;
   }
   print('Ogrencinin sonucu: $isSucess');
+
+  // magazaya gelen isimlerden cem olan varsa tebrik et
+  String name = "cem";
+  switch (name) {
+    case "cem":
+      print('Bravo');
+      break;
+    default:
+      print('hosgeldiniz');
+  }
+
+  //-------------
+
+  // String name = "ageagaga";
+  const String specialUser1 = "cem";
+  const String specialUser2 = "burak";
+
+  switch (name) {
+    case specialUser1:
+      print('Bravo');
+      break;
+    case specialUser2:
+      print('Merhaba $specialUser2');
+      break;
+    default:
+      print('hosgeldiniz');
+  }
 }

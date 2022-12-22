02ec3daf85d66185e0c7f849edaeb37c160cf6eb
Cem Kanpalta <ckanpalta@gmail.com>
2022-12-22 16:17:29 +0300

    Update class.dart
    Class yapisi ile ilgil yapilan ornek calismalar

--- L4/class.dart
+++ L4/class.dart
@@ -5,9 +5,40 @@ void main(List<String> args) {
   final int costumerAge = 30;
   //  yas 18 den buyukse islem yapabilir
 
-  if (costumerAge > 18) {
-    print('Merhaba, İşlem yapabilirsiniz');
+  costumerAgeControl(costumerAge);
+  print('**********');
+
+  // musterileri gruplayalim 25
+}
+
+void costumerAgeControl(int value) {
+  if (value > 18) {
+    print('Alişveriş yapabilirsiniz');
   } else {
-    print('İşlem yapamazsiniz');
+    print('Alisveris yapamazsiniz');
   }
+
+  // bankaya 3 musteri gelir
+  // birinin 100 tl var diğerinini hesabi yok 3 kisinin hesabinda para yok
+  // 100 lirasi olana hosgeldin hesabi olmayana hesap ac parasi olmayana is yapma
+  List<int?> costumersMoney = [100, null, 0];
+  for (var item in costumersMoney) {
+    if (item != null) {
+      if (item > 0) {
+        print('Hosgeldiniz');
+      } else {
+        print('bye');
+      }
+    } else {
+      print('Hesap açalim');
+    }
+  }
+}
+
+class User {
+  // ozelliklerini belirleyelim
+  String? name;
+  int? age;
+  int? money;
+  String? city;
 }
